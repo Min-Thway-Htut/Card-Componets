90d7c1a8559bc06a448a7383ef55438d2cdea38c
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-05-26 21:42:33 +0200

    First release of my project

---
+++ card-component/card-components/src/Card.jsx
@@ -0,0 +1,22 @@
+
+function Card(){
+    return(
+        <>
+        <div className="card">
+            
+        <img className = "card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title"></h2>
+            <p className="card-text">A second-year Computer Science student</p>
+        </div>
+
+          <div className="card">
+          <img className = "card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+          <h2 className="card-title">Min Thway Htut</h2>
+          <p className="card-text">A second-year Computer Science student</p>
+      </div>
+
+      </>
+    );
+}
+
+export default Card
